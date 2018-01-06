db7e000e64ec0347f79bb8d304f10da9a5149cc5
Mila Zangirolame <milazangirolame@gmail.com>
2018-01-06 01:29:08 -0200

    refactoring code and improving style

--- test/test.js
+++ test/test.js
@@ -1,27 +1,35 @@
 const assert = require('chai').assert ;
-const sayHello = require("../app").sayHello;
-const addNumbers = require("../app").addNumbers;
-//const app = require("../app");
+// const sayHello = require("../app").sayHello;
+// const addNumbers = require("../app").addNumbers;
+const app = require("../app");
+
+// Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
 
 describe("App", function(){
-  it("sayHello should return hello" , function(){
-    let result = sayHello();
-    assert.equal(result, "hello");
+  describe("sayHello()" , function(){
+    it("sayHello should return hello" , function(){
+      let result = app.sayHello();
+      assert.equal(sayHelloResult, "hello");
+    })
 
-  });
-
-  it("sayHello should be a string", function(){
-    let result = sayHello();
-    assert.typeOf(result, "string")
+    it("sayHello should be a string", function(){
+      let result = app.sayHello();
+      assert.typeOf(sayHelloResult, "string")
+    })
   })
 
-  it("addNumbers should be above 5" , function(){
-    let result = addNumbers(5,5);
-    assert.isAbove(result , 5)
+  describe("addNumbers()" , function(){
+    it("addNumbers should be above 5" , function(){
+      let result = app.addNumbers(5,5);
+      assert.isAbove(addNumbersResult , 5)
+    })
+
+    it("addNumbers should be a number" , function(){
+      let result = app.addNumbers(5,5);
+      assert.typeOf(addNumbersResult , "number")
+    })
   })
 
-  it("addNumbers should be a number" , function(){
-    let result = addNumbers(5,5);
-    assert.typeOf(result , "number")
-  })
 });
