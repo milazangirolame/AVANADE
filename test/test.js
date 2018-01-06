d6105bdc7b64d29bd6847bd4452beb857d342b8e
Mila Zangirolame <milazangirolame@gmail.com>
2018-01-06 01:30:38 -0200

    commenting result

--- test/test.js
+++ test/test.js
@@ -10,24 +10,24 @@ addNumbersResult = app.addNumbers(5,5);
 describe("App", function(){
   describe("sayHello()" , function(){
     it("sayHello should return hello" , function(){
-      let result = app.sayHello();
+      //let result = app.sayHello();
       assert.equal(sayHelloResult, "hello");
     })
 
     it("sayHello should be a string", function(){
-      let result = app.sayHello();
+      //let result = app.sayHello();
       assert.typeOf(sayHelloResult, "string")
     })
   })
 
   describe("addNumbers()" , function(){
     it("addNumbers should be above 5" , function(){
-      let result = app.addNumbers(5,5);
+      //let result = app.addNumbers(5,5);
       assert.isAbove(addNumbersResult , 5)
     })
 
     it("addNumbers should be a number" , function(){
-      let result = app.addNumbers(5,5);
+      //let result = app.addNumbers(5,5);
       assert.typeOf(addNumbersResult , "number")
     })
   })
